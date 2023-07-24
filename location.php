b9e73162d2f59da05a56cb1ae4b9bf736afd0007
Akhila11802 <anilkumarakhila80@gmail.com>
2023-07-24 07:01:32 +0530

    initial commit

---
+++ location.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+    <title>Location</title>
+    <style>
+        p:hover{
+        box-shadow: 30px 30px 50px -45px rgb(255, 217, 1);
+        transition: .5s ease-in-out;
+        cursor: pointer;
+    }
+    </style>
+    
+
+</head>
+
+<body  style="background: rgba(20, 19, 19, 0.884); ">
+    <p>
+        <br><br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.658860902959!2d72.82640651422831!3d19.383918497259966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7aec0a4b41bef%3A0x6954d52aa4e2f5f7!2sVIDYAVARDHINI%20COLLEGE%2C%20Vasai%20Rd%2C%20Vasai%20West%2C%20Vasai-Virar%2C%20Maharashtra%20401202!5e0!3m2!1sen!2sin!4v1651067653079!5m2!1sen!2sin" width="1000" height="520" style="border-width:3; border-color: rgb(160, 156, 134); padding:auto;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </p>
+</body>
+</html>
+
+
+
+
---
+++ team.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teams</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="stylesheet" href="myProjects/webProject/icofont/css/icofont.min.css">
+
+<link rel="stylesheet" href="teams.css">
+<body>
+    <section id = "team" class="team section-bg">
+        <div class = "container" data- aos = "fade-up">
+            <div class="section-title">
+                <h2>Our Team</h2>
+                <p>We listen, we discuss, we advise and develop. We love to learn and use the latest technologies.</p>
+                <!-- <br> -->
+                <p>Presenting you the Four magical hand behind 'Pettyqu'</p>
+            </div>
+            <div class = "row">
+                <div class = "col-lg-3 col-md-6 d-flex align-items stretch">
+                    <div class="member" data-aos = "fade-up" data-aos-delay = "100" >
+                        <div class="member-img">
+                            <img src="images/team_1_1.jpeg" class = "img-fluid" alt="">
+                            <div class="social">
+                                <a href=""><ion-icon name="logo-twitter"></ion-icon></a>
+                                <a href="https://www.facebook.com/profile.php?id=100073926732520"><ion-icon name="logo-facebook"></ion-icon></a>
+                                <a href="https://www.instagram.com/akhila_1810/"><ion-icon name="logo-instagram"></ion-icon></a>
+                                <a href="https://www.linkedin.com/in/akhila-anilkumar-a96197222/"><ion-icon name="logo-linkedin"></ion-icon></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Akhila Anilkumar</h4>
+                            <span>Computer Engineer</span>
+                        </div>
+                    </div>
+                </div>
+                <!--2nd-->
+                <div class = "col-lg-3 col-md-6 d-flex align-items stretch">
+                    <div class="member" data-aos = "fade-up" data-aos-delay = "200" >
+                        <div class="member-img">
+                            <img src="images/team_2_2.jpeg" class = "img-fluid" alt="">
+                            <div class="social">
+                                <a href=""><ion-icon name="logo-twitter"></ion-icon></a>
+                                <a href="https://www.facebook.com/polomi.adak"><ion-icon name="logo-facebook"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-instagram"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-linkedin"></ion-icon></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Polomi Adak</h4>
+                            <span>Computer Engineer</span>
+                        </div>
+                    </div>
+                </div>
+                <!--3rd-->
+                <div class = "col-lg-3 col-md-6 d-flex align-items stretch">
+                    <div class="member" data-aos = "fade-up" data-aos-delay = "300" >
+                        <div class="member-img">
+                            <img src="images/team_3_2.jpeg" class = "img-fluid" alt="">
+                            <div class="social">
+                                <a href=""><ion-icon name="logo-twitter"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-facebook"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-instagram"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-linkedin"></ion-icon></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Riya Khot</h4>
+                            <span>Computer Engineer</span>
+                        </div>
+                    </div>
+                </div>
+                <!--4th-->
+                <div class = "col-lg-3 col-md-6 d-flex align-items stretch">
+                    <div class="member" data-aos = "fade-up" data-aos-delay = "400" >
+                        <div class="member-img">
+                            <img src="images/team_4.jpeg" class = "img-fluid" alt="">
+                            <div class="social">
+                                <a href=""><ion-icon name="logo-twitter"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-facebook"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-instagram"></ion-icon></a>
+                                <a href=""><ion-icon name="logo-linkedin"></ion-icon></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Prerna Kanekar</h4>
+                            <span>Computer Engineer</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script>
+        let social = document.querySelector('.navigation');
+        navigation.onclick = function(){
+            navigation.classList.toggle('active')
+        }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
